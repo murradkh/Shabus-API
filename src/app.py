6fc1bcdf989dba0fd9c8c36e90c20a0ef9541ec6
murradkh <murradkhalil@gmail.com>
2018-05-01 23:03:06 +0300

    ..

--- src/app.py
+++ src/app.py
@@ -1,20 +1,21 @@
 from src.Common.Database import Database
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 app.config.from_object('src.config')
+app.secret_key = 'murrad'
+
 
 #
-# @app.before_first_request
-# def ini_db():
-#     Database.init_Database()
+@app.before_first_request
+def ini_db():
+    Database.init_Database()
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def home():
     return "ok"
 
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], port=4990)
-
