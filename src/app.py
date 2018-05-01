5e81abb7f28d1160a85008cf5a31a6d6899a99b1
murradkh <murradkhalil@gmail.com>
2018-05-01 22:35:14 +0300

    ..

--- src/app.py
+++ src/app.py
@@ -5,10 +5,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config.from_object('src.config')
 
-
-@app.before_first_request
-def ini_db():
-    Database.init_Database()
+#
+# @app.before_first_request
+# def ini_db():
+#     Database.init_Database()
 
 @app.route('/')
 def home():
