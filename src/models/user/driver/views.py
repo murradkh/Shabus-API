44cdb6ea707e5abe7e58b201c9cb36dbe0f31e2f
murradkh <murradkhalil@gmail.com>
2018-06-16 14:50:41 +0300

    ..

--- src/models/user/driver/views.py
+++ src/models/user/driver/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 
+from src.common.decorator import crossdomain
 from src.common.errors import CommonErrors
 from .constants import *
 from .driver import Driver
@@ -44,6 +45,7 @@ def logout():
 
 
 @Driver_blueprint.route('/forget-password', methods=['POST', 'OPTIONS'])
+@crossdomain(origin="http://localhost:8100/")
 def forget_password():
     if request.method == 'OPTIONS':
         return 'ok'
