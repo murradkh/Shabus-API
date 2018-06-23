16d150761c93ad49e44f39ed643e0d31c5b34990
murradkh <murradkhalil@gmail.com>
2018-06-23 22:02:39 +0300

    refinning...

--- src/models/user/manager/manager.py
+++ src/models/user/manager/manager.py
@@ -57,10 +57,9 @@ class Manager(object):
     def delete(collection):
         token, phone_number = Utils.check_json_vaild(request.get_json(), "Token", 'PhoneNumber')
         Utils.decode_token(token=token)
-        if collection == ('Passengers' or 'New rides'):
-            Database.delete(collection, {"phone_number": phone_number})
-        elif collection == 'Drivers':
+        if collection == 'Drivers':
             Database.delete(collection, {"PhoneNumber": phone_number})
             Database.delete_image(DB_COLLECION_IMAGES, {"PhoneNumber": phone_number})
         else:
             Database.delete(collection, {"PhoneNumber": phone_number})
+            Database.delete(collection, {"phone_number": phone_number})
